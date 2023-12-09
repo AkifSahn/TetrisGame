ea9d949524aef57aa37f11766294017e3fed6141
Akif <04makifsahin@gmail.com>
2023-12-09 17:15:36 +0300

    Piece is now rotateable. Changed shapeArr from Vector to dynamically created array

--- Piece.cpp
+++ Piece.cpp
@@ -2,20 +2,23 @@
 
 Piece::Piece()
 {
+
+    srand(time(0));
+
     w = 3;
     h = 3;
     x = 0;
     y = 0;
+    rotation = 0;
 
-    shapeArr = {0,
-                1,
-                0,
-                1,
-                1,
-                1,
-                0,
-                1,
-                0};
+    // shapeArr = new int[w * h];
+    stdIndex = rand() % 10;
+    shapeArr = standardPieces[stdIndex];
+}
+
+Piece::~Piece()
+{
+    delete shapeArr;
 }
 
 char Piece::returnPiece(int key)
@@ -28,6 +31,34 @@ char Piece::returnPiece(int key)
 
     default:
         return ' ';
+        break;
+    }
+}
+
+void Piece::rotatePiece(int r)
+{
+
+    switch (r)
+    {
+    case 0:
+        rotation = r;
+        shapeArr = standardPieces[stdIndex];
+        break;
+    case 90:
+        rotation = r;
+        int *tempArr = new int[w * h];
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                int index = i * w + j;
+                tempArr[index] = shapeArr[6 + i - 3 * j];
+            }
+        }
+        delete tempArr;
+        break;
+
+    default:
         break;
     }
 }
--- Piece.hpp
+++ Piece.hpp
@@ -1,20 +1,35 @@
 #include <time.h>
 #include <stdlib.h>
-#include <vector>
+#include <random>
 
 using std::vector;
+
+int standardPieces[10][9] = {{0, 1, 0, 1, 1, 1, 0, 1, 0},  // plus
+                             {1, 1, 0, 1, 1, 0, 0, 0, 0},  // square
+                             {1, 0, 0, 1, 1, 0, 0, 0, 0},  // corner
+                             {1, 0, 0, 1, 0, 0, 1, 0, 0},  // line
+                             {0, 1, 1, 1, 1, 0, 0, 0, 0},  // zig-zag
+                             {1, 0, 1, 1, 1, 1, 0, 0, 0},  // U piece
+                             {1, 0, 0, 1, 1, 0, 0, 1, 0},  // step piece
+                             {0, 1, 0, 1, 1, 1, 1, 0, 1},  // up arrow
+                             {1, 0, 0, 1, 1, 1, 0, 0, 1},  // Double zig-zag
+                             {0, 1, 0, 1, 1, 1, 0, 0, 0}}; // pyramid
 
 class Piece
 {
 public:
     // Create a piece instance with random pieceType
     Piece();
+    ~Piece();
     // Get a key of a piece and returns the according char.
     static char returnPiece(int);
+    // Rotate the piece clockwise to the given r(degree). r can take 0, 90, 180, 270
+    void rotatePiece(int r);
 
 private:
-    vector<int> shapeArr; // A 1D array simulating an 3x3 2D array.
-    // int rotation;         // 0 default, + rotates right, - rotates left
-    int w, h; // Shape of the simulated 2D array
-    int x, y; // Positions(representing the index in the current frame).
+    int *shapeArr; // A 1D array simulating an 3x3 2D array.
+    int rotation;  // 0, 90, 180, 270
+    int w, h;      // Shape of the simulated 2D array
+    int x, y;      // Positions(representing the index in the current frame).
+    int stdIndex;  // StandardPieces index of the piece
 };
